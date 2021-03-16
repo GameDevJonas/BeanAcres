37f75b3737ee06f19581917f8c12fc567972867f
Jonas <oddenjonas1@gmail.com>
2021-03-16 16:26:46 +0100

    Panning farming scene

---
+++ Assets/Scripts/CameraPanning.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPanning : MonoBehaviour
+{
+    Vector3 mousePos;
+    public Vector3 newMousePos;
+    public Vector3 clickedPos;
+
+    public Collider2D bounds;
+
+    public bool panning = false;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Input.mousePosition;
+        mousePos.z = 10;
+        newMousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        newMousePos.y = 0;
+    }
+
+    private void LateUpdate()
+    {
+        if (Input.GetMouseButton(0) && !panning)
+        {
+            clickedPos = newMousePos;
+            clickedPos.y = 0;
+            panning = true;
+        }
+        if (panning)
+        {
+            Camera.main.transform.Translate(clickedPos - newMousePos);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            panning = false;
+        }
+    }
+}
